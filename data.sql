d22cef26111cf89236a70925f7b7a564fb4b760c
Tim Palacios <timpalacios@u.boisestate.edu>
2025-08-18 20:34:05 -0600

    added data

---
+++ data.sql
@@ -0,0 +1,6 @@
+INSERT INTO tasks (id, title, done) VALUES (1, 'First task', 0);
+INSERT INTO tasks (id, title, done) VALUES (2, 'Second task', 1);
+INSERT INTO tasks (id, title, done) VALUES (3, 'Third task (edited)', 1);
+INSERT INTO tasks (id, title, done) VALUES (5, 'Milk', 0);
+INSERT INTO tasks (id, title, done) VALUES (6, 'Eggs', 0);
+INSERT INTO tasks (id, title, done) VALUES (7, 'Bacon', 1);
